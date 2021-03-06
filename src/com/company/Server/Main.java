e628505678bf27944b43c53bdaec69275a732c0b
Pavel <iamhere-lookatme@mail.ru>
2021-03-06 11:37:51 +0300

    ДЗ

---
+++ src/com/company/Server/Main.java
@@ -0,0 +1,9 @@
+package com.company.Server;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        new Server();
+    }
+}
